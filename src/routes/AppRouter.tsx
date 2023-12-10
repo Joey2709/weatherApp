101ff779bd1648adeb39aa4f9d2e49fc267f1d6c
Joseph <josephchero.jc@gmail.com>
2023-12-09 23:12:25 -0500

    Update routes in AppRouter

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -4,9 +4,9 @@ import Weather from "../pages/weather/Weather";
 const AppRouter = () => {
   return (
     <Routes>
-      <Route path="/*" element={<Navigate to="/" replace />} />
-      <Route path="/" element={<Weather />} />
-      <Route path="/:city_name" element={<Weather />} />
+      <Route path="/*" element={<Navigate to="/weather-app-react" replace />} />
+      <Route path="/weather-app-react" element={<Weather />} />
+      <Route path="/weather-app-react/:city_name" element={<Weather />} />
     </Routes>
   );
 };
